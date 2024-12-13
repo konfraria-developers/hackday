8395d400c670fc3ce6a06e0b8ebd4ee6386cdea3
Christian Adell <chadell@gmail.com>
2024-12-13 19:08:02 +0100

    deixar esquelet

--- main.py
+++ main.py
@@ -4,53 +4,16 @@ import os
 
 
 def obte_ciutat_i_temperatura():
-    # Demanar el nom de la ciutat
-    ciutat = input("Introdueix el nom de la ciutat: ")
-    
-    # Demanar la temperatura que l'usuari creu que hi ha
-    temperatura = float(input(f"Quina temperatura creus que hi ha a {ciutat}? "))
-
-    # Mostra el resultat
-    print(f"A {ciutat}, creus que la temperatura és de {temperatura} graus.")
-
-    return ciutat, temperatura
-    
+    """Retorna la ciutat i la temperatura que l'usuari ha introduït."""
+    return None, None
 
 def obtenir_temperatura(ciutat):
-    # Introdueix aquí la teva clau API d'OpenWeatherMap
-    api_key = os.getenv('API_KEY_OPENWEATHERMAP')
-    
-    base_url = "http://api.openweathermap.org/data/2.5/weather"
-    params = {
-        "q": ciutat,
-        "appid": api_key,
-        "units": "metric",  # Per obtenir la temperatura en graus Celsius
-    }
-    
-    # Realitzar la sol·licitud GET
-    resposta = requests.get(base_url, params=params)
-    
-    # Comprovar si la sol·licitud ha estat exitosa
-    if resposta.status_code == 200:
-        dades = resposta.json()
-        temperatura = dades["main"]["temp"]
-        print(f"La temperatura actual a {ciutat} és de {temperatura}°C.")
-    else:
-        print(f"No s'ha pogut obtenir la temperatura per a {ciutat}. Comprova que el nom de la ciutat sigui correcte.")
-        sys.exit(1)
-
-    return temperatura
+    """Retorna la temperatura actual a la ciutat especificada."""
+    pass
 
 def compara_temperatures(temperatura_real, temperatura_suposada, tolerancia=1):
-    # Calcula la diferència entre la temperatura real i la suposada
-    diferència = abs(temperatura_real - temperatura_suposada)
-    
-    # Comprova si la diferència està dins de la tolerància
-    if diferència <= tolerancia:
-        print(f"La temperatura suposada ({temperatura_suposada}°C) és CORRECTE dins d'una tolerància de ±{tolerancia}°C.")
-    else:
-        print(f"La temperatura suposada ({temperatura_suposada}°C) no és correcte.")
-
+    """Calcula la diferència entre la temperatura real i la suposada i comprova si està dins de la tolerància."""
+    pass
 
 def main():
     while True:
